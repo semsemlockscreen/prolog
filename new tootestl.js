e3ec68f077b41e213cd4f54bed9ae8234152f04a
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-09-22 20:39:13 +0100

    Update new tootestl.js

--- new tootestl.js
+++ new tootestl.js
@@ -11349,7 +11349,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
 	       changed = true ;
        }
       calculateActiveFingers();
-	 console.log('move fingers e.pointerID ' , e.pointerID)
+	 console.log('move fingers ' , fingers)
 	  
 
 		

ce9e992565c167a77418da62d66228e730528239
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-09-24 00:29:11 +0100

    Update new tootestl.js

--- new tootestl.js
+++ new tootestl.js
@@ -10994,6 +10994,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
 			cont.addChildAt(sbr, 3);
 			frameChafaf();
 		
+			 cont.cache(  0 , 0  , cont.getTransformedBounds().weight, cont.getTransformedBounds().height);		
 		});
 		
 		
@@ -11265,7 +11266,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
 				y: posY
 			};
 			
-	 cont.cache(  0 , 0  , cont.getTransformedBounds().weight, cont.getTransformedBounds().height);		
+	
 			
 		   if ( e.pointerID === -1) return ;//e.pointerID = -1;
       console.log('add fonger mousedown' , e.pointerID)

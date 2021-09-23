cc27d23c344a06d7adbedc833b8001f639fac949
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-09-23 13:27:04 +0100

    Update new tootestl.js

--- new tootestl.js
+++ new tootestl.js
@@ -10654,7 +10654,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
 		var isTouch = true;
 		
 		
-		alert('133');
+		alert('134');
 		
 		setTimeout(function () {
 		
@@ -11383,7 +11383,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
  
       if (fingers[evt.pointerID]) {
 	       console.log('deleted' , evt.pointerID);
-       fingers .splice(evt.pointerID , evt.pointerID);
+       fingers .splice(evt.pointerID ,1);
 	
       }
  

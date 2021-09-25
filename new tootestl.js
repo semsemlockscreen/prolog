faf8342b012584ff762a516b87e4ffeb53c4a102
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-09-25 13:57:39 +0100

    Update new tootestl.js

--- new tootestl.js
+++ new tootestl.js
@@ -11384,9 +11384,9 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
 		
 			if ((Math.abs(evt.currentTarget.down.x - pt.x) * scaleTouchFac) > 100 && !previous_x_update || (cont.isDragged && scaleTouchFac === 1 ))
 			{
-				console.log('evt.currentTarget.down.x - pt.x' , evt.currentTarget.down.x - pt.x)
-				console.log('scaleTouchFac' , scaleTouchFac)
-				console.log('evt.currentTarget.down.x - pt.x) * scaleTouchFac)',evt.currentTarget.down.x - pt.x) * scaleTouchFac);
+				console.log('evt.currentTarget.down.x - pt.x' , evt.currentTarget.down.x - pt.x);
+				console.log('scaleTouchFac' , scaleTouchFac) ;
+				console.log('evt.currentTarget.down.x - pt.x) * scaleTouchFac)', (evt.currentTarget.down.x - pt.x) * scaleTouchFac);
 				swapToNext(evt.currentTarget.down.x - pt.x, evt);
 			}
 			stage.update();

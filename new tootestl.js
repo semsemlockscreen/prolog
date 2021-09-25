c49a66b489aa2602bc9111b3aa4ddd28ba4fd98f
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-09-25 09:51:57 +0100

    Update new tootestl.js

--- new tootestl.js
+++ new tootestl.js
@@ -11278,7 +11278,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.30000000000001,102.1);
  
 	  
       calculateActiveFingers();
-   if(activeFingers === 2) cont.cache(  -8 , 0  , cont.getTransformedBounds().width, cont.getTransformedBounds().height);
+   if(activeFingers === 2) //cont.cache(  -10, 0  , cont.getTransformedBounds().width, cont.getTransformedBounds().height);
 		}
 		
 		

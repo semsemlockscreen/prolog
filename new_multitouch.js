47ab35fa05bd9c6919204756d8fc68c2580d9d6e
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-12-21 17:07:05 +0100

    Update new_multitouch.js

--- new_multitouch.js
+++ new_multitouch.js
@@ -10525,7 +10525,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.4,102);
 		startDrag();
 		cont.startDrag = startDrag;
 		
-		cont.addEventListener('touchstart', function (e) {
+		cont.addEventListener('mousedown', function (e) {
                      console.log('starrrrrrrrrrrrrrrrrr')
 		
 		})

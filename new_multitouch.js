036dcd25a2f849a692de2f729d7978be14ee635b
semsemlockscreen <70825069+semsemlockscreen@users.noreply.github.com>
2021-12-23 12:31:28 +0100

    Update new_multitouch.js

--- new_multitouch.js
+++ new_multitouch.js
@@ -10545,7 +10545,7 @@ p.nominalBounds = new cjs.Rectangle(-67.9,-51.6,87.4,102);
 		}
 				      )
 		
-		cont.addEventListener('touchmove', function (e) {
+		animation_container.addEventListener('touchmove', function (e) {
                      console.log('cont touchermove.addEventListener')
 			
 		}
